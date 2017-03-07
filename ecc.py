2f90859ddb25a8b75978a534aa5ca3301a338873
Bostjan <bostjan.vizintin@gmail.com>
2017-03-07 13:02:07 +0000

    first commit

---
+++ ecc.py
@@ -0,0 +1,56 @@
+import urllib
+import json
+import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+from helpers import *
+
+HASH = 'ij43mif5z8ec3w23z89jnv85e9o0p8e1'
+SERVER = '192.168.0.103'
+UPDATE_TIME = 60*1000
+SENSORBOX = []
+
+url = 'http://' + SERVER + '/pi/getSensorBoxData/' + HASH
+
+print "Printing url: "
+print url
+print "\n"
+
+
+try:
+	#print "Inside try statement\n"
+	response = urllib.urlopen(url).read()
+	SENSORBOX = json.loads(response)['sensorBox']
+except:
+	#print "Inside except statement\n"
+	sendError(101, HASH, SERVER)
+
+#print SENSORBOX
+
+numOfSensors = len(SENSORBOX['sensors'])
+values = [0]*numOfSensors
+updatedAt = [time.time()*1000]*numOfSensors
+numOfReadings = [0]*numOfSensors
+
+
+while(1):
+	#print "Inside while statement\n"
+	for i in range(0, numOfSensors):
+		#print "Inside first for loop\n"
+		values[i] += getSensorValue(SENSORBOX['sensors'][i]['input']-1, SENSORBOX['sensors'][i]['sampleTime'], SENSORBOX['sensors'][i]['sensorMvPerAmp'])
+		#print values[i]
+		numOfReadings[i]+=1
+	
+	for i in range(0, numOfSensors):
+		#print "inside second for loop\n"	
+		if(time.time()*1000 - updatedAt[i] > + UPDATE_TIME):
+			postMeasurement(SERVER, SENSORBOX['sensors'][i]['id'], values[i]/numOfReadings[i])
+			updatedAt[i] = time.time()*1000
+			numOfReadings[i] = 0
+			values[i] = 0
+			
+			
---
+++ helpers.py
@@ -0,0 +1,51 @@
+import time
+import urllib
+import urllib2
+from adc import *
+
+def sendError(errorCode, hash, server):
+	print errorCode
+	
+
+def delay(miliseconds):
+	startTime = time.time()*1000
+	while(1):
+		if(startTime< time.time()*1000 - miliseconds):
+			break
+
+def getSensorValue(port, sampleTime, mvPerAmp):
+	#print "mvPerAmp for sensor " + str(port) + " : " + str(mvPerAmp)
+	result = 0
+	readValue = 0
+	maxValue = 0
+	minValue = 1024
+	ampsRms = 0
+
+	startTime = time.time()*1000
+	while((time.time()*1000 - startTime) < sampleTime):
+		readValue = mcp.read_adc(port)
+		#print "ReadValue for " + str(port) + " : " + str(readValue)
+		if(readValue > maxValue):
+			maxValue =  readValue
+		elif(readValue < minValue):
+			minValue = readValue
+		result = ((maxValue - minValue) * 5.0) /1024.0
+		vrms = (result/2) * 0.707
+		ampsRms = (vrms * 1000) / mvPerAmp
+
+	#print "maxValue: " + str(maxValue)
+	#print "minValue: " + str(minValue)
+	#print "ampsRms: " + str(ampsRms)
+	return ampsRms
+
+def postMeasurement(server, idSensor, value):
+	#print "posting measurement"
+	if(value < 0.05):
+		#print "value is less than 0.05"
+		return
+	try:
+		url = 'http://' + server + '/pi/measurement/' + str(idSensor) + '/' + str(value)
+		response = urllib.urlopen(url).read()
+		print url
+	except:
+		sendError(102)
